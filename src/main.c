30f6a2df84cb2f74c5f6cd3bb0ca5d2bfddb1ac5
sinpistas <kale1216@gmail.com>
2026-02-07 21:23:48 +0100

    parser: parser included in main

--- src/main.c
+++ src/main.c
@@ -6,19 +6,32 @@
 /*   By: apestana <apestana@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 11:24:16 by apestana          #+#    #+#             */
-/*   Updated: 2026/02/07 17:19:07 by apestana         ###   ########.fr       */
+/*   Updated: 2026/02/07 20:07:29 by apestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	cub_usage(void)
+{
+	printf("Usage: ./cub3D <map.cub>\n");
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_scene	sc;
 
-	(void)argc;
-	(void)argv;
+	if (argc != 2)
+		return (cub_usage());
 	cub_scene_init(&sc);
+	if (cub_parse_file(&sc, argv[1]) != 0)
+	{
+		printf("Error\n");
+		cub_scene_free(&sc);
+		return (1);
+	}
+	printf("OK\n");
 	cub_scene_free(&sc);
 	return (0);
 }
