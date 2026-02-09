1dee45349b521d13c784c6c3271dae47b7853dd7
sinpistas <kale1216@gmail.com>
2026-02-09 13:31:53 +0100

    Added a clamp for perp_wall_dist and limited draw_start/draw_end

--- src/game/raycast_utils.c
+++ src/game/raycast_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycast_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancanale <antonioayr.94@gmail.com>         +#+  +:+       +#+        */
+/*   By: apestana <apestana@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 13:00:00 by ancanale          #+#    #+#             */
-/*   Updated: 2026/02/08 17:10:16 by ancanale         ###   ########.fr       */
+/*   Updated: 2026/02/09 13:09:42 by apestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@ void	init_ray(t_game *game, t_ray *ray, int x)
 	ray->ray_dir_y = game->cam.dir_y + game->cam.plane_y * ray->camera_x;
 	ray->map_x = (int)game->cam.pos_x;
 	ray->map_y = (int)game->cam.pos_y;
-	ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
-	ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
 	ray->hit = 0;
 }
 
@@ -60,11 +66,19 @@ void	calc_wall_height(t_game *game, t_ray *ray)
 	else
 		ray->perp_wall_dist = (ray->map_y - game->cam.pos_y
 				+ (1 - ray->step_y) / 2) / ray->ray_dir_y;
+	if (ray->perp_wall_dist < 1e-6)
+		ray->perp_wall_dist = 1e-6;
 	ray->line_height = (int)(WIN_HEIGHT / ray->perp_wall_dist);
+	if (ray->line_height < 1)
+		ray->line_height = 1;
 	ray->draw_start = -ray->line_height / 2 + WIN_HEIGHT / 2;
 	if (ray->draw_start < 0)
 		ray->draw_start = 0;
+	if (ray->draw_start >= WIN_HEIGHT)
+		ray->draw_start = WIN_HEIGHT - 1;
 	ray->draw_end = ray->line_height / 2 + WIN_HEIGHT / 2;
+	if (ray->draw_end < 0)
+		ray->draw_end = 0;
 	if (ray->draw_end >= WIN_HEIGHT)
 		ray->draw_end = WIN_HEIGHT - 1;
 }
@@ -90,9 +104,10 @@ void	calc_texture_x(t_game *game, t_ray *ray, t_img *texture)
 
 void	perform_dda(t_game *game, t_ray *ray)
 {
-	int	len;
+	int	guard;
 
-	while (ray->hit == 0)
+	guard = game->scene.map_h * game->scene.map_w + 1;
+	while (ray->hit == 0 && guard-- > 0)
 	{
 		if (ray->side_dist_x < ray->side_dist_y)
 		{
@@ -107,12 +122,11 @@ void	perform_dda(t_game *game, t_ray *ray)
 			ray->side = 1;
 		}
 		if (ray->map_y < 0 || ray->map_y >= game->scene.map_h
-			|| ray->map_x < 0)
-			break ;
-		len = ft_strlen(game->scene.map[ray->map_y]);
-		if (ray->map_x >= len)
+			|| ray->map_x < 0 || ray->map_x >= game->scene.map_w)
 			break ;
 		if (game->scene.map[ray->map_y][ray->map_x] == '1')
 			ray->hit = 1;
 	}
+	if (ray->hit == 0)
+		ray->hit = 1;
 }
