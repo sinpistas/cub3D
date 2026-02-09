6bb101839f7ee1fb714aff03f9664f6c757d663f
sinpistas <kale1216@gmail.com>
2026-02-09 13:33:40 +0100

    Fix: The movement is only updated if there is displacement

--- src/game/events_update.c
+++ src/game/events_update.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events_update.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancanale <antonioayr.94@gmail.com>         +#+  +:+       +#+        */
+/*   By: apestana <apestana@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 13:00:00 by ancanale          #+#    #+#             */
-/*   Updated: 2026/02/08 17:09:45 by ancanale         ###   ########.fr       */
+/*   Updated: 2026/02/09 13:11:19 by apestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,47 @@
 
 void	handle_movement_keys(t_game *game, int *moved)
 {
-	if (game->keys.w)
+	double	old_x;
+	double	old_y;
+
+	if (game->keys.w || game->keys.s || game->keys.a || game->keys.d)
 	{
-		move_forward(game);
-		*moved = 1;
-	}
-	if (game->keys.s)
-	{
-		move_backward(game);
-		*moved = 1;
-	}
-	if (game->keys.a)
-	{
-		move_left(game);
-		*moved = 1;
-	}
-	if (game->keys.d)
-	{
-		move_right(game);
-		*moved = 1;
+		old_x = game->cam.pos_x;
+		old_y = game->cam.pos_y;
+		if (game->keys.w)
+			move_forward(game);
+		if (game->keys.s)
+			move_backward(game);
+		if (game->keys.a)
+			move_left(game);
+		if (game->keys.d)
+			move_right(game);
+		if (game->cam.pos_x != old_x || game->cam.pos_y != old_y)
+			*moved = 1;
 	}
 }
 
 void	handle_rotation_keys(t_game *game, int *moved)
 {
-	if (game->keys.left)
+	double	old_dir_x;
+	double	old_dir_y;
+	double	old_plane_x;
+	double	old_plane_y;
+
+	if (game->keys.left || game->keys.right)
 	{
-		rotate_left(game);
-		*moved = 1;
-	}
-	if (game->keys.right)
-	{
-		rotate_right(game);
-		*moved = 1;
+		old_dir_x = game->cam.dir_x;
+		old_dir_y = game->cam.dir_y;
+		old_plane_x = game->cam.plane_x;
+		old_plane_y = game->cam.plane_y;
+		if (game->keys.left)
+			rotate_left(game);
+		if (game->keys.right)
+			rotate_right(game);
+		if (game->cam.dir_x != old_dir_x || game->cam.dir_y != old_dir_y
+			|| game->cam.plane_x != old_plane_x
+			|| game->cam.plane_y != old_plane_y)
+			*moved = 1;
 	}
 }
 
